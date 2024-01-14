fb79d0d8e86c9c1f7f59f761b070166eb6d7203f
Kabraeshwar <142217878+Kabraeshwar@users.noreply.github.com>
2024-01-15 00:59:16 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,165 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.3/font/bootstrap-icons.min.css">
+ 
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<style>
+      h1 {text-align: center;margin:2%;}
+      p {text-align: center; margin:1%;padding:3%;}
+      div {text-align: center;}
+
+      
+   </style>
+<body>
+
+    <header>
+         <section id="topbar" class="mb-2 mb-lg-0 mb-sm-0 d-none d-lg-flex align-items-center pt-2 pb-2 bg-primary text-white topbar-transparent">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6   text-start">
+             <span class="px-3"><i class="bi bi-phone "></i>+91 9529422852</span>
+          <i class="bi bi-clock"></i> Mon-Sat: 9:30 AM - 3:00 PM
+            </div>
+            <div class="col-md-6 text-end">
+                <a href="https://www.facebook.com/jijauschoolandjr.college/" class="me-4 text-reset">
+                    <i class="bi bi-facebook"></i>
+                </a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="newindex.php">
+                    <h2 class="fw-bold  mb-2 mb-lg-0 mb-sm-0">Jijau School & Jr. College</h2>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="bi bi-list"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav m-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="newindex.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="about.php">About Us</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="mission.php">Our Mission</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="vision.php">Our Vision</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="gallery.php">Our Gallery</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="courses.php">Our Courses</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="eform.php">Apply Now</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.php">Contact us</a>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav mb-2 mb-lg-0 action-menu">
+                        <li class="nav-item">
+                            <a class="nav-link " href="#">
+                                
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <section>
+        <h1>About Us</h1>
+        <p style = "font-size:25px;">We are a happy school that provides a safe and nurturing environment of learning which enables all our children to achieve the skills needed for adult life. Through a stimulating engaging curriculum that encompasses indoor learning opportunities, we insure that every day counts at Jijau School & Jr. College. We pride ourselves on our Values Based Education which results in a welcoming and caring ethos. We are all learners and our key values underpin our success, inspiring children, parents, staff and governors to learn and grow together. We enjoy working closely with parents and recognise the importance of establishing strong home/school partnerships. We understand that the health and safety of all our children is of paramount importance so our school provides a safe environment in which our children can flourish. We hope you will enjoy our school you get a flavor for the opportunities we provide at “Jijau School & Jr. College Shelgaon”.</p>
+</section>
+
+
+
+
+    <footer class="text-center text-lg-start bg-primary py-3 text-white">
+            <section class="">
+                <div class="container text-center text-md-start mt-5">
+                    <div class="row mt-3">
+                        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                            <h6 class="text-uppercase fw-bold mb-4">
+                                <i class="bi bi-gem me-3"></i>Jijau School & Junior College 
+                            </h6>
+                            <p>
+                                We are one of the leading school known for providing #1 World Class Education
+                            </p>
+                        </div>
+
+                        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                            <h6 class="text-uppercase fw-bold mb-4">
+                                Useful links
+                            </h6>
+                            <p>
+                                <a href="about.php" class="text-reset">About Us</a>
+                            </p>
+                            <p>
+                                <a href="gallery.php" class="text-reset">Our Gallery</a>
+                            </p>
+                            <p>
+                                <a href="eform.php" class="text-reset">Apply Now</a>
+                            </p>
+                            <p>
+                                <a href="contact.php" class="text-reset">Contact Us</a>
+                            </p>
+                        </div>
+
+                        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+                            <h6 class="text-uppercase fw-bold mb-4">
+                                Contact
+                            </h6>
+                            <p><i class="bi bi-location me-3"></i>Shelgaon, Tq. Badnapur Dist. Jalna</p>
+                            <p>
+                                <i class="bi bi-envelope me-3"></i>
+                                jijauschool@gmail.com
+                            </p>
+                            <p><i class="bi bi-phone me-3"></i> 9529422852</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <div class="text-center py-4" style="background-color: rgba(0, 0, 0, 0.05);">
+                © 2023 Copyright:
+                <a class="text-reset fw-bold" href="newindex.php">Jijau School and Junior College, Shelgaon</a>
+            </div>
+        </footer>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+            crossorigin="anonymous"></script>
+
+</body>
+
+</html>
